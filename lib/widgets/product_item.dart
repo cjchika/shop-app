31b37a05fbbe0e81acb3ded3ac077bd1f686e0ec
Chijioke Chika <8pluscreative@gmail.com>
2022-08-22 12:45:41 +0100

    Building UI

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../screens/product_detail_screen.dart';
 import '../providers/product.dart';
+import '../providers/cart.dart';
 
 class ProductItem extends StatelessWidget {
   // final String id;
@@ -13,7 +14,8 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context);
+    final cart = Provider.of<Cart>(context, listen: false);
     return Consumer<Product>(
       builder: (ctx, product, child) => ClipRRect(
         borderRadius: BorderRadius.circular(10),
@@ -33,12 +35,18 @@ class ProductItem extends StatelessWidget {
               product.title,
               textAlign: TextAlign.center,
             ),
-            trailing: const IconButton(
+            trailing: IconButton(
               icon: Icon(
-                Icons.shop,
+                Icons.shopping_cart,
                 color: Colors.indigoAccent,
               ),
-              onPressed: null,
+              onPressed: () {
+                cart.addItem(
+                  product.id,
+                  product.price,
+                  product.title,
+                );
+              },
             ),
           ),
           child: GestureDetector(
