4d0530e9b99604aad8408c149ab5d2ebaea7344f
Chijioke Chika <8pluscreative@gmail.com>
2022-08-22 20:36:29 +0100

    Building UI/UX

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -4,6 +4,7 @@ import '../widgets/badge.dart';
 import '../providers/cart.dart';
 
 import '../widgets/products_grid.dart';
+import '../screens/cart_screen.dart';
 
 enum FilterOptions {
   Favorites,
@@ -55,7 +56,9 @@ class _ProductOverviewPageState extends State<ProductOverviewPage> {
               icon: Icon(
                 Icons.shopping_cart,
               ),
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context).pushNamed(CartScreen.routePath);
+              },
             ),
           )
         ],
