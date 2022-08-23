c72677886e7d09b5a462c03657a6e4a135060a76
Chijioke Chika <8pluscreative@gmail.com>
2022-08-23 16:25:11 +0100

    Building UI/UX

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -21,6 +21,42 @@ class ProductDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(loadedProduct.title),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '\$${loadedProduct.price}',
+              style: TextStyle(
+                color: Colors.indigoAccent,
+                fontSize: 25,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              width: double.infinity,
+              child: Text(
+                loadedProduct.description,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
