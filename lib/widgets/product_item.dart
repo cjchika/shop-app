614e28712ed4576c955e02dc3e10ffec9a3d8bfb
Chijioke Chika <8pluscreative@gmail.com>
2022-08-24 08:53:44 +0100

    Building UI/UX

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -41,10 +41,21 @@ class ProductItem extends StatelessWidget {
                 color: Colors.indigoAccent,
               ),
               onPressed: () {
-                cart.addItem(
-                  product.id,
-                  product.price,
-                  product.title,
+                cart.addItem(product.id, product.price, product.title);
+                ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      'Added item to the cart!',
+                    ),
+                    duration: Duration(seconds: 2),
+                    action: SnackBarAction(
+                      label: 'UNDO',
+                      onPressed: () {
+                        cart.removeSingleItem(product.id);
+                      },
+                    ),
+                  ),
                 );
               },
             ),
